b9f716880abdf28ba9f56cbcd5c81b5c2be3203d
victor-singha <vicunite2@gmail.com>
2021-05-04 09:41:33 +0530

    [victor] lambda function added

--- RegexApp/Pattern.cs
+++ RegexApp/Pattern.cs
@@ -9,30 +9,22 @@ namespace RegexApp
 {
     public class Pattern
     {
-        string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
-        string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
-        string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-        string REGEX_MOBILE = "^[0-9]{2}[ ][1-9][0-9]{9}$";
-        string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
-        public bool validateFirstName(string input)
-        {
-            return Regex.IsMatch(input, REGEX_FIRSTNAME);
-        }
-        public bool validateLastName(string input)
-        {
-            return Regex.IsMatch(input, REGEX_LASTNAME);
-        }
-        public bool validateEmail(string input)
-        {
-            return Regex.IsMatch(input, REGEX_EMAIL);
-        }
-        public bool validateMobile(string input)
-        {
-            return Regex.IsMatch(input, REGEX_MOBILE);
-        }
-        public bool validatePassword(string input)
-        {
-            return Regex.IsMatch(input, REGEX_PASSWORD);
-        }
+        static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
+        static string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        static string REGEX_MOBILE = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+        static string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
+        
+        public Func<string, bool> validateFirstName = (input) =>
+        { return Regex.IsMatch(input, REGEX_FIRSTNAME); };
+        public Func<string, bool> validateLastName = (input) =>
+        { return Regex.IsMatch(input, REGEX_LASTNAME); };
+        public Func<string, bool> validateEmail = (input) =>
+        { return Regex.IsMatch(input, REGEX_EMAIL); };
+        public Func<string, bool> validateMobile = (input) =>
+        { return Regex.IsMatch(input, REGEX_MOBILE); };
+        public Func<string, bool> validatePassword = (input) =>
+        { return Regex.IsMatch(input, REGEX_PASSWORD); };
+
     }
 }
