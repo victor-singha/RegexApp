205fa42ee9c13f1ac9e5154059bade3805dfcf31
victor-singha <vicunite2@gmail.com>
2021-05-04 11:36:20 +0530

    [victor] const keyword

--- RegexApp/Pattern.cs
+++ RegexApp/Pattern.cs
@@ -9,11 +9,11 @@ namespace RegexApp
 {
     public class Pattern
     {
-        static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
-        static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
-        static string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-        static string REGEX_MOBILE = "^[0-9]{2}[ ][1-9][0-9]{9}$";
-        static string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
+        const string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        const string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
+        const string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        const string REGEX_MOBILE = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+        const string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
         
         public Func<string, bool> validateFirstName = (input) =>
         { return Regex.IsMatch(input, REGEX_FIRSTNAME); };
